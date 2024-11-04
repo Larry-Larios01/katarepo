51a010929ba89c4916a86b942c6bbf739d361b25
larry123 <larioslarry8@gmail.com>
2024-11-04 14:49:31 -0600

    add more funtions to the player to verify if is allie, attack, leave and , add to more Fractions

--- Objects.py
+++ Objects.py
@@ -3,20 +3,62 @@ class Player:
         self.Name = Name
         self.level = level
         self.status = status
-        self.Faction = Faction
+        self.Faction = []
         self.health = health
 
 
-    def getattack(self, attacker_level):
-        if self.level - attacker_level >= 5 :
+    def getattack(self, attacker):
+        if self.level - attacker.level >= 5 :
             self.health = self.health * 0.5
 
-        elif attacker_level - self.level >=5:
+        elif attacker.level - self.level >=5:
             self.health = self.health * 1.5
 
         if self.health <=0 :
             self.health = 0
             self.status = 0
 
-            
+     
+
+    def isallie(self, victim):
+        for Faction in self.Faction:
+            if Faction in victim.Faction:
+                return True
+            else:
+                return False
+    
+    
+    
+    def Attack(self, victim ):
+        flag = self.isallie(self, victim)
+        if flag: 
+            return print("you Cant damage him is your allie")
+        else:
+            self.getattack(victim, self)
+      
+        
+
+    
+
+    def EnterInAFaction(self, Faction):
+        if Faction in self.Faction:
+            return print("you are aleredy in this faction")
+        
+        else:
+            self.Faction.append(Faction)
+
+
+
+    def LeaveFaction(self, Faction):
+        if Faction in self.Faction:
+            return print("you are not in this Faction")
+        else :
+            self.Faction.remove(Faction)
+
+
+
+    
+
+
+
 
