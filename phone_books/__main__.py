d978732f269e961208313896d8b8367b782ca74e
larry123 <larioslarry8@gmail.com>
2024-11-26 11:56:43 -0600

    addinf the function post

--- phone_books/__main__.py
+++ phone_books/__main__.py
@@ -7,7 +7,8 @@ from starlette.config import Config
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
-
+config = Config('.env')
+DATABASE_URL = config('DATABASE_URL')
 
 metadata = sqlalchemy.MetaData()
 
@@ -19,3 +20,28 @@ notes = sqlalchemy.Table(
     sqlalchemy.Column("email", sqlalchemy.String),
     sqlalchemy.Column("phone", sqlalchemy.String),
 )
+
+database = databases.Database(DATABASE_URL)
+
+async def insert_user(request):
+    data = await request.json()
+    query = notes.insert().values(
+       name=data["name"],
+       email=data["email"],
+       phone=data["phone"]
+    )
+    await database.execute(query)
+    return JSONResponse({
+       "name": data["name"],
+       "email": data["email"],
+       "phone": data["phone"],
+    })
+
+
+routes = [
+    Route("/contacts/", endpoint=list_notes, methods=["GET"]),
+    Route("/contacts", endpoint=insert_user, methods=["POST"]),
+    Route("/contacts/{id}", endpoint=list_notes, methods=["GET"]),
+    Route("/contacts/{id}", endpoint=list_notes, methods=["PATCH"]),
+    Route("/contacts/{id}", endpoint=list_notes, methods=["DELETE"]),
+]
