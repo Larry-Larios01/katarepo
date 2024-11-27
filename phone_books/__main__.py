bf14019ce30d6b2d524f93728a26d2f04434a767
larry123 <larioslarry8@gmail.com>
2024-11-27 10:35:38 -0600

    adding req from get user

--- phone_books/__main__.py
+++ phone_books/__main__.py
@@ -12,6 +12,7 @@ import uvicorn
 from typing import TypedDict
 from http.client import HTTPResponse
 from psycopg2.extras import RealDictCursor
+from starlette.requests import Request
 
 load_dotenv(verbose=True)
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -23,60 +24,19 @@ def get_connection():
 
 
 
-class insert_user_param(TypedDict):
+class Contact(TypedDict):
     name:str
     email: str
     phone: str
 
-
-def from_req(request) -> insert_user_param:
-    data = request.json()
-    return insert_user_param(name=data["name"], email=data["email"], phone=data["phone"])
-
-async def insert_user_handler(params: insert_user_param):
-    query = notes.insert().values(name=params["name"], email=params["email"], phone=params["phone"])
-    await database.execute(query)
-    return params
-
-def to_res(user: insert_user_param) -> JSONResponse:
-    return JSONResponse({"name": user["name"], "email": user["email"], "phone": user["phone"]})
-
-async def insert_user(request):
-    params = from_req(request)
-    result = await get_user_handler(params)
-    return to_res(result)
+class GetResult():
+    Contacts: list[Contact]
 
 
-def from_req(request) -> int:
-    data = request.path_params.get("id")
-    return data
 
-async def get_user_handler(params: int):
-    query = notes.select().where(notes.c.id == params)
-    contact = await database.fetch_one(query)
-    return contact
+def from_req_get_user(request: Request)-> int:
+    pass
 
-def to_res(user) -> JSONResponse:
-    return JSONResponse(dict(user))
-
-async def list_notes(request):
-    
-    note_id = request.path_params.get("id")
-    
-    note_id = int(note_id)
-    
-    query = notes.select().where(notes.c.id == note_id)
-    result = await database.fetch_one(query)
-
-
-    content = {
-        "id": result["id"],
-        "name": result["name"],
-        "email": result["email"],
-        "phone": result["phone"],
-
-    }
-    return JSONResponse(content)
 
 
 
@@ -95,7 +55,7 @@ async def get_user(request):
 routes = [
     #Route("/contacts/", endpoint=list_notes, methods=["GET"]),
     Route("/contacts", endpoint=insert_user, methods=["POST"]),
-    Route("/contacts/{id}", endpoint=list_notes, methods=["GET"]),
+    Route("/contacts/{id}", endpoint=get_user, methods=["GET"]),
     #Route("/contacts/{id}", endpoint=list_notes, methods=["PATCH"]),
     #Route("/contacts/{id}", endpoint=list_notes, methods=["DELETE"]),
 ]
