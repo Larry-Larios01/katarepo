79f13940acdf6c1c86f5cb2208283466e0346978
larry123 <larioslarry8@gmail.com>
2024-11-28 11:38:09 -0600

    adding the filter functions

--- phone_books/__main__.py
+++ phone_books/__main__.py
@@ -204,8 +204,83 @@ async def delete_user(request: Request):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def from_req_filter_contacts(request: Request)-> Contact:
+    user_id = request.path_params.get("id")
+    dic_to_update = {"id":f"{user_id}"}
+    data = request.json()
+    allowed_parameters = ("name", "email", "phone")
+    for key,value in data.items():
+        if key in allowed_parameters:
+             dic_to_update[key] = value
+        
+    return dic_to_update
+
+async def filter_contacts_handler(params: dict)-> int:
+    conn = await get_connection()
+    id = params.pop("id", None)
+    set_keys = []
+    set_values = []
+    for i, (key, value) in enumerate(params.items(), start=1):
+        set_keys.append(f"{key} = ${i}")
+        set_values.append(value)
+
+    set_values.append(id)
+    set_keys_as_text = ", ".join(set_keys)
+
+
+                
+    async with conn.cursor() as c:
+            
+            
+            result = await c.execute(
+            f"""
+            UPDATE users
+            SET {set_keys_as_text}
+            WHERE id = ${len(set_values)}
+            RETURNING id, name, email, phone
+            """, set_values
+        )
+            user = await result.fetchone()
+            contact = Contact(id=user["id"],name=user['name'], email=user['email'], phone=user["phone"])
+    await conn.close()
+            
+    return contact
+
+
+def to_res_filter_contacts(user: Contact)-> JSONResponse:
+     return JSONResponse({
+        "id": f"{user["id"]}",
+        "message": "was succesful inserted"
+    })
+
+async def filter_contacts(request: Request):
+    params = from_req_partial_update(request)
+    result = await partial_update_handler(params)
+    return to_res_partial_update(result)
+
+
+
 routes = [
-    #Route("/contacts/", endpoint=list_notes, methods=["GET"]),
+    Route("/contacts/", endpoint=filter_contacts, methods=["GET"]),
     Route("/contacts", endpoint=insert_user, methods=["POST"]),
     Route("/contacts/{id}", endpoint=get_user, methods=["GET"]),
     Route("/contacts/{id}", endpoint=partial_update, methods=["PATCH"]),
