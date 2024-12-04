a17247894507d132282c8f796c2e305fa4eddc61
larry123 <larioslarry8@gmail.com>
2024-12-04 10:11:44 -0600

    starting teh cronometer

---
+++ views/vue-project/src/components/cronometer.vue
@@ -0,0 +1,26 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const interval = ref(setInterval(start,1000))
+
+    function start() {
+      
+    }
+
+    // don't forget to expose the function as well.
+    return {
+      interval
+    }
+  }
+}
+
+
+
+
+</script>
+
+<template>
+    <p>{{ interval }}</p>
+</template>
