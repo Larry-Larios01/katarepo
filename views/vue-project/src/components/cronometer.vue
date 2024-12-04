1c9e7fca9c00970abab3e93da3c195da70419188
larry123 <larioslarry8@gmail.com>
2024-12-04 11:38:06 -0600

    adding pause and lap

--- views/vue-project/src/components/cronometer.vue
+++ views/vue-project/src/components/cronometer.vue
@@ -1,27 +1,39 @@
-<script>
+<script lang="ts">
 import { ref } from 'vue'
 
 export default {
   setup() {
     
     const count = ref(0)
-    let cronometer = null
+    let cronometer : number;
+    const numbers = ref<number[]>([]);
     function start() {
     cronometer = setInterval(() => {
     count.value++;
         }, 1000)
     }
 
-    function stop() {
+    function pause() {
     clearInterval(cronometer)
+    }
+
+    function stop() {
+    count.value = 0
+    clearInterval(cronometer)
+    }
+
+    function lap() {
+        numbers.value.push(count.value)
+    
     }
 
     // don't forget to expose the function as well.
     return {
       start,
       count,
-      cronometer,
-      stop
+      stop,
+      pause, 
+      lap
     }
   }
 }
@@ -33,6 +45,13 @@ export default {
 
 <template>
     <button @click="start"> start </button>
-    <button @click="stop">stop</button>
+    <button @click="pause">pause</button>
+    <button @click="stop"> stop </button>
+    <button @click="lap">lap</button>
     <p>{{ count }}</p>
+
+    <li v-for="lap in numbers">
+  {{ lap }}
+</li>
+
 </template>
