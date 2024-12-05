783314a8a0deb8e47d6720ce2db4de1e8e9648a6
larry123 <larioslarry8@gmail.com>
2024-12-05 09:59:47 -0600

    addig the prop laps

--- views/vue-project/src/components/cronometer.vue
+++ views/vue-project/src/components/cronometer.vue
@@ -1,58 +1,57 @@
 <script lang="ts">
-import { ref , defineComponent} from 'vue'
+import { ref, defineComponent } from 'vue';
 
 export default defineComponent({
+  name: 'cronometer',
 
-    name: 'cronometer',
-  
-  setup() {
-    
-    const count = ref(0)
-    let cronometer : number;
+ 
+  setup() { // Props es recibido aquí
+    const count = ref(0);
+    let cronometer: number;
     const numbers = ref<number[]>([]);
+
     function start() {
-    cronometer = setInterval(() => {
-    count.value++;
-        }, 1000)
+      cronometer = setInterval(() => {
+        count.value++;
+      }, 1000);
     }
 
     function pause() {
-    clearInterval(cronometer)
+      clearInterval(cronometer);
     }
 
     function stop() {
-    count.value = 0
-    clearInterval(cronometer)
-    numbers.value = []
+      count.value = 0;
+      clearInterval(cronometer);
+      numbers.value = [];
     }
 
     function lap() {
-        numbers.value.push(count.value)
-    
+      numbers.value.push(count.value);
     }
 
-    // don't forget to expose the function as well.
+    // Ahora puedes usar props.laps
+    const evaluate = () => numbers.value.length < props.laps;
+
+    // Exponer las funciones y propiedades al template
     return {
       start,
       count,
       stop,
-      pause, 
+      pause,
       lap,
-      numbers
-    }
-  }
-})
-
-
-
-
+      numbers,
+      evaluate,
+    };
+  },
+});
 </script>
 
 <template>
     <button @click="start"> start </button>
     <button @click="pause">pause</button>
     <button @click="stop"> stop </button>
-    <button @click="lap">lap</button>
+    <button v-if="evaluate()" @click="lap">lap</button>
     <p>{{ count }}</p>
 
     <li v-for="lap in numbers">
