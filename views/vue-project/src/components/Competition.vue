1688be0c305e5bd7b71e535c7ebf150f43ffa15a
larry123 <larioslarry8@gmail.com>
2024-12-05 14:09:39 -0600

    adding the inputs of the competition

--- views/vue-project/src/components/Competition.vue
+++ views/vue-project/src/components/Competition.vue
@@ -20,17 +20,49 @@ export default defineComponent({
   setup() {
 
 
-    
+   const inputs = ref<String[]>([]);
+
+
+
+   function submitValues()
+   {
+    console.log(inputs)
+
+   }
+
+
+   
+
    
 
 
 
     // don't forget to expose the function as well.
     return {
+      inputs
  
     }
   }
-})
+});
+
+</script>
+
+
+<template>
+  <div>
+    <div v-for="(input, index) in inputs" :key="index">
+      <label :for="'input-' + index">Input {{ index + 1 }}</label>
+      <input
+        type="text"
+        v-model="inputs[index]"
+        placeholder="Enter value"
+      />
+    </div>
+    <button v-on:click="submitValues()>Submit</button>
+  </div>
+</template>
+
+
 
 
 
